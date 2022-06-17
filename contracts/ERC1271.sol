d5d008bda8e4196b67bd960e013897483a1287e9
Ewan Humbert <xeway@protonmail.com>
2022-06-17 15:32:51 +0200

    Addded ERC1271.sol

---
+++ contracts/ERC1271.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// please refer to https://eips.ethereum.org/EIPS/eip-1271
+
+contract ERC1271 {
+
+    function isValidSignature(
+        bytes32 _hash,
+        bytes calldata _signature,
+        address _signer
+    ) public view returns (bool) {
+        return recoverSigner(_hash, _signature) == _signer ? true : false;
+    }
+
+    function recoverSigner(
+        bytes32 _hash,
+        bytes memory _signature
+    ) internal pure returns (address signer) {
+        require(_signature.length == 65, "SignatureValidator#recoverSigner: invalid signature length");
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            r := mload(add(_signature, 32))
+            s := mload(add(_signature, 64))
+            v := byte(0, mload(add(_signature, 96)))
+        }
+
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            revert("SignatureValidator#recoverSigner: invalid signature 's' value");
+        }
+
+        if (v != 27 && v != 28) {
+            revert("SignatureValidator#recoverSigner: invalid signature 'v' value");
+        }
+
+        signer = ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)),
+            v,
+            r,
+            s
+        );
+
+        require(
+            signer != address(0x0),
+            "SignatureValidator#recoverSigner: INVALID_SIGNER"
+        );
+
+        return signer;
+    }
+
+}
